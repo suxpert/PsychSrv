6c8807cb874998b12b3eb9a47eda019e665427c2
LiTuX Sol <LiTuX@live.com>
2022-12-22 22:41:13 +0800

    update dataset code

--- rdm_dataset.py
+++ rdm_dataset.py
@@ -6,6 +6,7 @@ from __future__ import division
 import os
 import sys
 import numpy as np
+import yaml
 
 from psychopy import core
 from psychopy import visual
@@ -21,17 +22,46 @@ def savestim(frames, filename, coherence=None, direction=None):
 
 
 def main():
-    # some initialization
+    # configuration:
+    config = {
+        'folder': 'dataset-6',
+        'signaldots': 'different',
+        'noisedots': 'direction',
+        'direction': ['left', 'right'],
+        'coherence': list(range(100)),
+        'repeat': 10,
+        'ndots': 200,
+        'dotsize': 6,
+        'dotlife': 4,
+        'speed': 2,
+        'dim': [300, 300],
+        'nframes': 120,
+        'bgcolor': 'black',
+        'color': 'white',
+    }
 
+    # some initialization
     cwd = os.path.dirname(os.path.abspath(__file__))
     os.chdir(cwd)
 
+
+    folder = config['folder']
+    if not os.path.isdir(folder):
+        os.mkdir(folder)
+
+    cfgfile = os.path.join(folder, '_meta.txt')
+    if os.path.exists(cfgfile):
+        raise FileExistsError('Config file exist! dataset is not empty!')
+    
+    with open(cfgfile, 'w') as file:
+        yaml.dump(config, file)
+
     win = visual.Window(
-        size=(300,300),
+        size=config['dim'],
         fullscr=False,
         screen=0,
         allowGUI=True,
-        color=[-1,-1,-1],
+        color=config['bgcolor'],
         colorSpace='rgb',
         units='pix',
         monitor='testMonitor',
@@ -51,23 +81,25 @@ def main():
         font='Arial',
         color='white',
         pos=(0, 0),
-        height=48,
+        height=24,
     )
 
-    text_instr.setText('Space or Enter\nto start')
+    text_instr.setText('Space or Enter to start')
     text_instr.draw()
     win.flip()
 
     keybd.waitKeys(keyList=['space', 'enter'])
     
-    # for c in range(100):
-    for c in [60, 50, 40, 30, 20, 10]:
-        for r in range(10):
-            for a in [0, 180]:
-                if a == 0:
-                    d = 'right'
-                else:
-                    d = 'left'
+    # for c in [60, 50, 40, 30, 20, 10]:
+    for c in config['coherence']:
+        for d in config['direction']:
+            if d == 'left':
+                a = 180
+            elif d == 'right':
+                a = 0
+
+            for r in range(config['repeat']):
+            
                 text_instr.setText(f'c{c}{d[0]}-{r}')
                 text_instr.draw()
                 win.flip()
@@ -75,26 +107,26 @@ def main():
 
                 dots = visual.DotStim(
                     win=win,
-                    signalDots='same',
-                    noiseDots='direction',
-                    nDots=200,
-                    dotSize=6,
-                    dotLife=4,
-                    speed=2,
+                    signalDots=config['signaldots'],
+                    noiseDots=config['noisedots'],
+                    nDots=config['ndots'],
+                    dotSize=config['dotsize'],
+                    dotLife=config['dotlife'],
+                    speed=config['speed'],
                     dir=a,
                     coherence=c/100,
                     fieldPos=(0, 0),
                     fieldSize=270,
                     fieldAnchor='center',
                     fieldShape='circle',
-                    color='white',
+                    color=config['color'],
                 )
 
                 # skip the very first frame?
                 # dots.draw()
                 # win.flip()
 
-                for ff in range(fps*2):
+                for ff in range(config['nframes']):
                     dots.draw()
                     win.flip()
                     win.getMovieFrame()
@@ -102,7 +134,7 @@ def main():
                         core.quit()
 
                 win.flip()
-                savestim(win.movieFrames, f'data/c{c}{d[0]}-{r}.npz', c, d)
+                savestim(win.movieFrames, f'{folder}/c{c}{d[0]}-{r}.npz', c, d)
                 win.movieFrames = []
 
 
