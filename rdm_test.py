8253019da39ef165cec9bdefaa12a3c25af41cbd
LiTuX Sol <LiTuX@live.com>
2022-12-22 23:11:14 +0800

    update main script

--- rdm_test.py
+++ rdm_test.py
@@ -6,10 +6,13 @@ from __future__ import division
 import os
 import sys
 import numpy as np
+import random
 
 from psychopy import core
+from psychopy import event
 from psychopy import visual
-from psychopy import data #, event, logging, clock
+from psychopy import data
+# logging, clock
 
 from psychopy.hardware import keyboard
 
@@ -24,39 +27,105 @@ def savestim(frames, filename, coherence=None, direction=None):
     np.savez_compressed(filename, stim=movie, coherence=coherence, direction=direction)
 
 
+def cleanup(win=None, httpd=None):
+    if httpd is not None:
+        httpd.stop()
+    if win is not None:
+        win.close()
+
+    core.quit()
+
+
+def randcoh(level, ntrials, factor=None):
+    adapt = [
+        [-1,  1] * 2,
+        [-1,  1] * 3,
+        [-1,  1] * 4,
+        [-1,  1] * 5,
+        # [-1,  1] * 6,
+        [-1,  0,  1] * 2,
+        [-1,  0,  1] * 3,
+        [-1,  0,  1] * 4,
+        [-1,  0,  1] * 5,
+        [-1,  0,  1] * 6,
+        [-3, -1,  1,  3] * 2,
+        [-3, -1,  1,  3] * 3,
+        [-3, -1,  1,  3] * 4,
+        [-3, -1,  1,  3] * 5,
+        [-2, -1,  0,  1,  2] * 2,
+        [-2, -1,  0,  1,  2] * 3,
+        [-2, -1,  0,  1,  2] * 4,
+        [-5, -3, -1,  1,  3,  5] * 2,
+        [-5, -3, -1,  1,  3,  5] * 3,
+        [-3, -2, -1,  0,  1,  2,  3] * 2,
+        # [-7, -5, -3, -1,  1,  3,  5,  7],
+        # [-5, -3, -2, -1,  0,  1,  2,  3, 5],
+    ]
+    scale = [
+        [],
+        [5,15],
+        [3,8],
+        [2,5],
+        [1,4],
+        [1,3],
+    ]
+    cands = [l for l in adapt if len(l) == ntrials]
+    if len(cands) == 0:
+        cands = [l for l in adapt if len(l) == ntrials+1]
+    if len(cands) == 0:
+        cands = [l for l in adapt if len(l) == ntrials+2]
+
+    choose = random.choice(cands)
+    
+    if factor is None:
+        tmp = scale[max(choose)]
+        factor = np.random.randint(tmp[0],tmp[1])
+    coherence = level + np.array(choose) * factor
+    return coherence
+
+
 def main():
     # some initialization
 
     cwd = os.path.dirname(os.path.abspath(__file__))
     os.chdir(cwd)
 
+    if not os.path.isdir('run'):
+        os.mkdir('run')
+
     win = visual.Window(
-        size=(400,400),
+        size=(500,500),
         fullscr=False,
         screen=0,
-        allowGUI=False,
-        color=[-1,-1,-1],
+        # allowGUI=False,
+        color='black',
         colorSpace='rgb',
         units='pix',
         monitor='testMonitor',
     )
     win.mouseVisible = True
+    # on my computer, only iohub is fully compatible with pyautoui
     keybd = keyboard.Keyboard(backend='iohub')
 
     expinfo = {
         'date': data.getDateStr(),
-        'name': os.path.basename(__file__),
+        'name': os.path.basename(__file__)[:-3],
         'cwd': cwd,
         'fps': win.getActualFrameRate(),
     }
+    result = {
+        'trial': [],
+        'coherence': [],
+        'direction': [],
+        'response': [],
+        'rt': [],
+        'correct': [],
+    }
 
     print(f'Refresh rate = {expinfo["fps"]} Hz')
-    if expinfo['fps'] is None:
-        # fall back to 60Hz
-        expinfo['fps'] = 60
-        dur = 1/60
-    else:
-        dur = 1 / round(expinfo['fps'])
+    # always assume a 60Hz screen, the above fps always change
+    expinfo['fps'] = 60
+    dur = 1/60
 
     # prepare components
     # static ISI and fixation
@@ -68,7 +137,7 @@ def main():
         size=(10, 10),
         ori=0,
         pos=(0, 0),
-        anchor='center',
+        # anchor='center',
         lineWidth=0,
         lineColor=None,
         fillColor='red',
@@ -78,11 +147,11 @@ def main():
     text_instr = visual.TextStim(
         win=win,
         name='instr',
-        text='',
+        text='Preparing...',
         font='Arial',
         color='white',
         pos=(0, 0),
-        height=48,
+        height=36,
     )
 
     # we can reuse instruction as countdown
@@ -96,55 +165,65 @@ def main():
         height=48,
     )
 
-    text_instr.setText('Preparing...')
-    text_instr.draw()
+    text_instr.setAutoDraw(True)
     win.flip()
 
     # setup server and wait for connection
     httpd = setupserver()
     httpd.start()
 
-    text_instr.setText(f'Waiting at\n{httpd.server_addr}')
-    text_instr.draw()
-    win.flip()
+    event.globalKeys.clear()
+    event.globalKeys.add(key='escape', func=cleanup, func_kwargs={'win': win, 'httpd': httpd})
 
+    text_instr.setText(f'Listening at {httpd.server_addr}')
+
+    # ugly waiting for connection
     while True:
+        win.flip()
         core.wait(0.1)
-        mesg, state = httpd.get_state(False)
-        if mesg != 'init':
-            print(mesg, state)
+        query, param = httpd.get_query(True)
+        if query == 'get: /':
+            # some one is requesting the home page
             break;
+        if keybd.getKeys(keyList=['escape']):
+            cleanup(win=win, httpd=httpd)
 
-    # httpd.handle_request()
-    # keybd.waitKeys(keyList=['space', 'enter'])
-    text_instr.setText(f'connected.\nWaiting configuration')
-    text_instr.draw()
-    win.flip()
+    text_instr.setText('Connected. Waiting configuration')
+    # text_instr.draw()
+    # win.flip()
 
+    # don't know why, this one does not work
     # keybd.waitKeys(keyList=['space', 'enter'])
     
-    while True:
-        core.wait(0.1)
-        mesg, state = httpd.get_state(False)
-        # print(mesg, state)
-        if mesg == 'post: /':
-            break;
-
     # configuration from remote
-    # mesg, state = httpd.get_state(False)
-    print(mesg, state)
+    while True:
+        win.flip()
+        core.wait(0.1)
+        query, param = httpd.get_query(True)
+        if query == 'post: /':
+            break;
+        if keybd.getKeys(keyList=['escape']):
+            cleanup(win=win, httpd=httpd)
 
-    config = {
-        'signal': 'same',
-        'noise': 'direction',
-        'ndots': 300,
-        'size': 5,
-        'life': 3,
-        'speed': 3,
-        'ntrials': 8,
-        'duration': 2,
-    }
-    config = state
+    # config = {
+    #     'signal': 'same',
+    #     'noise': 'direction',
+    #     'ndots': 300,
+    #     'size': 5,
+    #     'life': 3,
+    #     'speed': 3,
+    #     'ntrials': 8,
+    #     'duration': 2,
+    # }
+    config = param
+
+    ntrials = config['ntrials']
+    text_instr.setText(f'Will run {ntrials} trials')
+    # text_instr.draw()
+    win.flip()
+
+    core.wait(0.5)
+    text_instr.setAutoDraw(False)
 
     # the dot stimulus have to be create after configuration
     dots = visual.DotStim(
@@ -159,7 +238,7 @@ def main():
         dir=0,
         coherence=0.5,
         fieldPos=(0, 0),
-        fieldSize=300,
+        fieldSize=270,
         fieldAnchor='center',
         fieldShape='circle',
         color='white',
@@ -176,20 +255,26 @@ def main():
         win.flip()
 
     # prepare trial order and loop for trials
-    for trial in range(config['ntrials']):
-        httpd.set_state('trial', {'left', config['ntrials']-trial-1})
-
+    coherence = randcoh(config['coherence'], ntrials)
+    for trial in range(ntrials):
         # the trial:
         realdir = ['left', 'right']
         thisdir = np.random.randint(2)
-        thiscoh = 0.5
+        thiscoh = coherence[trial]
 
         dots.dir = 180 * (1 - thisdir)
-        dots.coherence = thiscoh
+        dots.coherence = thiscoh / 100
 
         win.callOnFlip(keybd.clock.reset)
         win.callOnFlip(keybd.clearEvents)
         win.callOnFlip(timer.reset)
+        win.callOnFlip(httpd.set_state,
+            state="stim",
+            trial=trial,
+            remain=ntrials-trial-1,
+            direction=realdir[thisdir],
+            coherence=int(thiscoh),
+        )
 
         nframes = int(config['duration']/dur)
         for ff in range(nframes):
@@ -197,31 +282,51 @@ def main():
             fixation.draw()
             win.flip()
             win.getMovieFrame()
-        pass
+
         fixation.draw()
         t = win.flip()
-#        print(t, timer.getTime())
-        isi.start(0.5)
 
-#        savestim(win.movieFrames, f'run/trial-{trial}.npz', thiscoh, realdir[thisdir])
+        isi.start(0.5)
+        httpd.set_state(
+            state="isi",
+            trial=trial,
+            remain=ntrials-trial-1,
+        )
+
+        savestim(win.movieFrames, f'run/trial-{trial}.npz', thiscoh, realdir[thisdir])
         # win.saveMovieFrames(f'dots-{trial}.gif', fps=expinfo['fps'])
         win.movieFrames = []
 
         isi.complete()
         resp = keybd.getKeys(keyList=['left', 'right'])
         # resp = keybd.waitKeys(0.5, keyList=['left', 'right'], clear=False)
+
         if len(resp) == 0:
-            print('miss!')
+            ch = ''
+            rt = np.nan
+            fb = 'miss'
+            print(f'trial {trial}: miss!', realdir[thisdir])
         else:
-            print(realdir[thisdir], resp[-1].name, resp[-1].rt)
-            if resp[-1].name == realdir[thisdir]:
-                print('correct')
+            ch = resp[-1].name
+            rt = resp[-1].rt
+            if ch == realdir[thisdir]:
+                fb = 'correct'
+            else:
+                fb = 'error'
+            print(f'trial {trial}: {fb}', realdir[thisdir], ch, rt)
         # keybd.clearEvents()
+        result['trial'].append(trial)
+        result['coherence'].append(thiscoh)
+        result['direction'].append(realdir[thisdir])
+        result['response'].append(ch)
+        result['rt'].append(rt)
+        result['correct'].append(fb)
 
     # after all trials done: report some results
-    pass
-    httpd.stop()
+    np.savez(f'run/{expinfo["name"]}-{expinfo["date"]}.npz', **result)
+    cleanup(win=win, httpd=httpd)
 
 
 if __name__ == '__main__':
     main()
+
