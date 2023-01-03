121c22cad26bcedcf5ef374defdc5970bc2c8ed9
LiTuX Sol <LiTuX@live.com>
2023-01-03 14:38:04 +0800

    refactorize psychoserver with psychopy window and http server

--- PsychoServer.py
+++ PsychoServer.py
@@ -1,61 +1,27 @@
 #!/usr/bin/env python3
 
 
+from __future__ import division
+
 import socket
 import json
 import pyautogui
 import threading
+import time
+import io
 
 from http.server import SimpleHTTPRequestHandler, HTTPServer
-# from psychopy import core
+
+from psychopy import core
+from psychopy import event
+from psychopy import visual
+
+# from psychopy.hardware import keyboard
 
 
-class MyHTTPServer(HTTPServer):
-    def __init__(self, server_address, RequestHandlerClass, bind_and_activate=True):
-        super().__init__(server_address, RequestHandlerClass, bind_and_activate)
-        self.server_addr = '%s:%d' % (socket.gethostbyname(self.server_name), self.server_port)
-        self.server_thread = threading.Thread(target=self.serve_forever)
-        self.state = {'state': 'init'}
-        self.query = ('', {})
-    
-    def set_state(self, **kwargs):
-        if kwargs:
-            print('state = ', kwargs)
-        self.state = kwargs
-    
-    def set_query(self, query='', **kwargs):
-        if kwargs:
-            print(f"query = '{query}', ", kwargs)
-        self.query = (query, kwargs)
-
-    def get_query(self, clear=False):
-        query, param = self.query
-        if clear:
-            self.query = ('', {})
-        return query, param
-
-    def run(self):
-            try:
-                self.serve_forever()
-            except KeyboardInterrupt:
-                pass
-            finally:
-                # Clean-up server (close socket, etc.)
-                self.server_close()
-
-    def start(self):
-        self.server_thread.start()
-
-    def stop(self):
-        self.shutdown()
-        self.server_thread.join()
-
-
-class MyRequestHandler(SimpleHTTPRequestHandler):
+class PsychoRequestHandler(SimpleHTTPRequestHandler):
     def _set_headers(self, type='.html'):
         self.send_response(200)
-        # self.send_header('Accept', 'application/json')
-        # self.send_header('Content-type', 'application/json')
         self.send_header('Content-type', self.extensions_map[type])
         self.end_headers()
 
@@ -65,6 +31,19 @@ class MyRequestHandler(SimpleHTTPRequestHandler):
             state = json.dumps(self.server.state)
             self._set_headers('.json')
             self.wfile.write(state.encode("utf-8"))
+        elif self.path == '/get_frame' or self.path == '/get_gray':
+            # return a png image
+            frame = self.server.get_frame()
+            if self == '/get_gray':
+                frame = frame.convert('L')
+            if frame is not None:
+                with io.BytesIO() as buff:
+                    frame.save(buff, format='png')
+                    self._set_headers('.png')
+                    self.wfile.write(buff.getvalue())
+            else:
+                self._set_headers('.html')
+
         else:
             SimpleHTTPRequestHandler.do_GET(self)
 
@@ -94,20 +73,154 @@ class MyRequestHandler(SimpleHTTPRequestHandler):
         self.server.set_query(f'post: {self.path}', **param)
 
 
-def setupserver(addr='0.0.0.0', port=8080):
-    return MyHTTPServer((addr, port), MyRequestHandler)
+class PsychoServer(HTTPServer):
+    def __init__(self, size, fullscr=False, port=8080, verbose=True):
+        super().__init__(('0.0.0.0', port), PsychoRequestHandler)
+        self.server_addr = '%s:%d' % (socket.gethostbyname(self.server_name), self.server_port)
+        self.server_thread = threading.Thread(target=self.serve_forever)
+        self.state = {'state': 'init'}
+        self.query = ('', {})
+        self.verbose = verbose
+
+        self.win = visual.Window(
+            size=size,
+            fullscr=fullscr,
+            screen=0,
+            allowGUI=False,
+            color='black',
+            colorSpace='rgb',
+            units='pix',
+            monitor='testMonitor',
+        )
+        self.win.mouseVisible = True
+
+        self.fps = self.win.getActualFrameRate()
+        self.timer = core.Clock()
+
+        self._info = visual.TextStim(
+            win=self.win,
+            text='Initializing',
+            font='Arial',
+            color='white',
+            pos=(0, 0),
+            height=36,
+        )
+
+        self._fixation = visual.ShapeStim(
+            win=self.win,
+            vertices='cross',
+            size=(10, 10),
+            ori=0,
+            pos=(0, 0),
+            # anchor='center',
+            lineWidth=0,
+            lineColor=None,
+            fillColor='red',
+        )
+
+        event.globalKeys.clear()
+        event.globalKeys.add(key='escape', func=self.__del__)
+
+        self.show_info('Initializing')
+        self.flip()
+
+    def start(self):
+        '''
+        async serve forever in background
+        '''
+        self.server_thread.start()
+
+    def stop(self):
+        '''
+        stop the serve forever thread
+        '''
+        self.shutdown()
+        self.server_thread.join()
+
+    def __del__(self):
+        self.stop()
+        self.win.close()
+        core.quit()
+
+    def run(self):
+        self.start()
+        while True:
+            q, p = self.get_query()
+            if q:
+                self.show_info(q)
+                self.flip(True)
+            # self.win.winHandle.activate()
+            time.sleep(0.1)
+
+    def set_state(self, **kwargs):
+        '''
+        set server state, which can be fetched using http get
+        '''
+        if self.verbose and kwargs:
+            print('state = ', kwargs)
+        self.state = kwargs
+
+    def get_state(self):
+        return self.state
+
+    def wait_state(self, state, interval=0.1):
+        while True:
+            if self.state['state'] == state:
+                return self.server.state
+            time.sleep(interval)
+
+    def set_query(self, query='', **kwargs):
+        '''
+        set request infomation, to be used in request callback
+        '''
+        if self.verbose and kwargs:
+            print(f"query = '{query}', ", kwargs)
+        self.query = (query, kwargs)
+
+    def get_query(self, clear=False):
+        '''
+        get the last request info
+        '''
+        query, param = self.query
+        if clear:
+            self.query = ('', {})
+        return query, param
+
+    def wait_query(self, query, interval=0.1):
+        while True:
+            q, param = self.get_query(True)
+            if q == query:
+                return param
+            time.sleep(interval)
+
+    def flip(self, cache=False):
+        self.win.flip()
+        if cache:
+            self.win.getMovieFrame()
+
+    def get_frame(self):
+        if len(self.win.movieFrames) > 0:
+            return self.win.movieFrames.pop(0)
+        else:
+            return None
+
+    def clear_cache(self):
+        self.win.movieFrames = []
+
+    def show_info(self, info):
+        if info:
+            self._info.setText(info)
+            self._info.setAutoDraw(True)
+        else:
+            self._info.setAutoDraw(False)
+
+    def show_fixation(self):
+        self._fixation.draw()
 
 
 def main():
-    httpd = setupserver()
-    httpd.run()
-
-#    httpd.start()
-#    for ii in range(20):
-#        query, param = httpd.get_query()
-#        core.wait(0.5)
-#
-#    httpd.stop()
+    ps = PsychoServer(size=(500,500), port=8080)
+    ps.run()
 
 
 if __name__ == '__main__':
