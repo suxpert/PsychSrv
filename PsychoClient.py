4b0e217871be58ff90b82d03478a51567b7b0f4f
LiTuX Sol <suxpert@gmail.com>
2023-01-05 10:55:27 +0800

    add get frame function

---
+++ PsychoClient.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+
+
+from __future__ import division
+
+import requests
+import time
+import numpy as np
+
+from PIL import Image
+
+
+class PsychoClient():
+    def __init__(self, ip, port, verbose=True):
+        self.address = f'http://{ip}:{port}'
+        self.verbose = verbose
+
+    def get_state(self, timeout=0.2):
+        url = self.address + '/status'
+        try:
+            req = requests.get(url, timeout=timeout)
+            if req.ok and req.headers['Content-Type'] == 'application/json':
+                state = req.json()
+            else:
+                state = {
+                    'code': req.status_code,
+                    'content': req.content,
+                }
+        except Exception as e:
+            state = str(e)
+
+        return state
+
+    def wait_state(self, state, interval=0.2, timeout=0.2):
+        url = self.address + '/status'
+        while True:
+            try:
+                req = requests.get(url, timeout=timeout)
+                if self.verbose:
+                    print(req.content)
+                if req.ok and req.headers['Content-Type'] == 'application/json':
+                    ret = req.json()
+                    if 'state' in ret and ret['state'] == state:
+                        return ret
+            # except requests.ConnectionError as e:
+            except Exception as e:
+                if self.verbose:
+                    print(e)
+            time.sleep(interval)
+
+    def get_frame(self, timeout=0.2):
+        # url = self.address + '/frame'
+        url = self.address + '/image'
+        try:
+            req = requests.get(url, stream=True, timeout=timeout)
+            print(req.headers)
+            if req.ok and req.headers['Content-Type'] == 'image/png':
+                req.raw.decode_content = True
+                img = Image.open(req.raw, formats=['png'])
+                frame = np.array(img)
+            else:
+                if self.verbose:
+                    print(req.content)
+                frame = None
+        except Exception as e:
+            if self.verbose:
+                print(e)
+            frame = None
+
+        return frame
+
+    def loop_frame(self, callback: callable, interval=0.1, timeout=0.2):
+        # url = self.address + '/frame'
+        url = self.address + '/image'
+        while True:
+            try:
+                req = requests.get(url, stream=True, timeout=timeout)
+                if req.ok and req.headers['Content-Type'] == 'image/png':
+                    req.raw.decode_content = True
+                    img = Image.open(req.raw, formats=['png'])
+                    frame = np.array(img)
+                    callback(frame)
+                else:
+                    if self.verbose:
+                        print(req.content)
+                    time.sleep(interval)
+            except Exception as e:
+                if self.verbose:
+                    print(e)
+                time.sleep(interval)
+
+
+def main():
+    client = PsychoClient('127.0.0.1', 8080)
+    while True:
+        state = client.get_state()
+        print(state)
+        time.sleep(2)
+    pass
+
+
+if __name__ == '__main__':
+    main()
